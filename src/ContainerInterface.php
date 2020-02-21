dc8845ef03c99722718f7ea5e2179f7302e279d0
Johannes Tegnér <johannes@jitesoft.com>
2020-02-21 09:45:17 +0100

    Added 'singleton' arg to container set.

--- src/ContainerInterface.php
+++ src/ContainerInterface.php
@@ -12,12 +12,12 @@ use Psr\Container\ContainerInterface as PsrContainerInterface;
  * @package Jitesoft\Contracts
  */
 interface ContainerInterface extends PsrContainerInterface {
-
     /**
      * Add or replace a value in the container.
      *
-     * @param string $key   Key which the value can be accessed by.
-     * @param mixed  $value Value to add.
+     * @param string $abstract  Abstract key to bind to.
+     * @param mixed  $concrete  Concrete value to generate.
+     * @param bool   $singleton If only a single instance should be created or a new one on each get-call.
      */
-    public function set(string $key, $value): void;
+    public function set(string $abstract, $concrete, bool $singleton = false): void;
 }
